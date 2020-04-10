5f7b8048061e21449cb3d4645548c253be1ed4c3
rappos <emilemail4@gmail.com>
2020-04-10 10:48:12 +0200

    Updates

---
+++ precache-manifest.8ca2017b394b7384813b67cfca0e4231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d213cc8e5ac987a5c528351fc36179b",
+    "url": "/winnerback-songs/index.html"
+  },
+  {
+    "revision": "4b900aa80083564a1fbb",
+    "url": "/winnerback-songs/static/js/2.d5f680bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winnerback-songs/static/js/2.d5f680bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053268d5215171094e26",
+    "url": "/winnerback-songs/static/js/main.134b2997.chunk.js"
+  },
+  {
+    "revision": "1067659c3929d0e1b5e0",
+    "url": "/winnerback-songs/static/js/runtime-main.283626bc.js"
+  }
+]);
